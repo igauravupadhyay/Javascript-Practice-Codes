57bbfc7e023781de662367ec5e3cb86d59be2211
GAURAV UPADHYAY <gauravupadhyay1006@gmail.com>
2024-12-04 11:31:38 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all Js code as newer version
+
+//alert("hi, gaurav");    //we are using nodejs not browser
+// alert(3 + 3)
+
+
+console.log(3 + 3); 
+                            //code readabilty should be high
+console.log("hi, gaurav")
+
+let name = "hitesh"
+let isLoggedIn = "true"
+let age = 54
+let state;
+
+//datatypes > primitives
+// number => 2 to the power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value  (for eg. jab hum tempertaure maangte hain current aur server work nhi kr rha hain to hamein 0 de sakta hai jo ki galat hai kyunki 0 bhi ek temp. hai.isliye jab server mein kuch gadbad ho to usne hsmein null return krna chahiye)
+// undefined => 
+
+//Non primitives
+//objects
+
+console.log(typeof age);
+console.log(typeof "hitesh");
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
+
+
+
+
+
+
+
